b1a40b1541ac23a9f901a5b9e838788e033f1598
Osman Mert ACAR <osman.acar@getir.com>
2023-07-09 14:55:24 +0300

    seenDate

--- lambda/timeline.ts
+++ lambda/timeline.ts
@@ -147,6 +147,7 @@ export const handler: Handler = async (
           secondary: iterator.secondary,
         },
         newTweet: answer,
+        seenDate: new Date().toISOString(),
       },
     });
 
